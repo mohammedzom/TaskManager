f4e3f0fc8dd7b8f99aa5d81ade91b63b117ae579
mohammedzom <mohammedzomlot2@gmail.com>
2025-11-20 19:45:28 +0200

    Stop Using Manual CRUD Routes! Switch to Resource Controllers

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('tasks', [TaskController::class, 'index']);
-Route::post('tasks', [TaskController::class, 'store']);
-Route::put('tasks/{id}', [TaskController::class, 'update']);
-Route::get('tasks/{id}', [TaskController::class, 'show']);
-Route::delete('tasks/{id}', [TaskController::class, 'destroy']);
+Route::apiResource('tasks', TaskController::class);
